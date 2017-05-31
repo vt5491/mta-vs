1123d383611b1a209c04a98b5c8a3d1d138859f5
vt5491 <vt5491@gmail.com>
2017-05-31 01:18:44 -0700

    minor fixes found during usage.

--- src/mta-vs-ext.ts
+++ src/mta-vs-ext.ts
@@ -13,6 +13,7 @@ exports MTA_VS;
 export function activate(context: vscode.ExtensionContext) {
 
     console.log(`MtaVsExt.activate: __dirname=${__dirname}`);
+    console.log(`MtaVsExt.activate: context.storagePath=${context.storagePath}`);
     
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
